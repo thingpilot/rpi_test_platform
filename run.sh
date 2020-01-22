4bad588370c316cb1326363210526201e1c1b370
Adam <adam@think-engineer.com>
2020-01-22 12:29:13 +0000

    Allow time for master SIO server to start

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 sudo /home/pi/miniconda3/envs/rpi-test-platform/bin/python app.py &
-python module_detect.py &
-python ocd/ocd_gpio.py
+sleep 3s
+python ocd/ocd_gpio.py &
+sleep 3s
+python module_detect.py
