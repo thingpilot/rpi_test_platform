9460bbcff513f7bfc1221b69d0ce09d9d70941d9
Adam <adam@think-engineer.com>
2020-01-22 11:43:29 +0000

    Upload firmware files to /ocd/firmware

--- app.py
+++ app.py
@@ -6,25 +6,68 @@
 """
 
 # Standard library imports
-from os import urandom
+from os import urandom, path, getcwd
 
 # 3rd-party library imports
 import eventlet
-from flask import Flask, render_template, escape, request, url_for
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, escape, request, url_for, redirect, flash, Response
+from flask_socketio import SocketIO
+from werkzeug.utils import secure_filename
 
 # Thingpilot library imports
 import app_utils
 
 # Global Flask and SocketIO objects
+ALLOWED_EXTENSIONS = {'bin'}
+UPLOAD_FOLDER = '/ocd/firmware'
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = f"{urandom(64)}"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 socketio = SocketIO(app, async_mode='eventlet')
+
+
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/firmware', methods=['POST'])
+def firmware_upload():
+    if request.method == 'POST':
+        print("POST POST POST POST POST")
+        print(getcwd())
+
+        if 'file' not in request.files:
+            flash('File not in request.files!', 'danger')
+            return redirect(request.url)
+        
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No file selected!', 'danger')
+            return redirect(request.url)
+
+        if allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(f"{getcwd()}{path.join(app.config['UPLOAD_FOLDER'], filename)}")
+            print(path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(f"{getcwd()}{path.join(app.config['UPLOAD_FOLDER'], filename)}")
+            return Response(status=201)
 
 
 @app.route('/developer')
