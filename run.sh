fecd0ec0a218c7ba55a36673c467aadda66660da
Adam <adam@think-engineer.com>
2020-01-24 13:27:38 +0000

    Don't call ocd_wrapper in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 sudo /home/pi/miniconda3/envs/rpi-test-platform/bin/python app.py &
 sleep 7s
 python gpio_manager.py &
-sudo /home/pi/miniconda3/envs/rpi-test-platform/bin/python ocd/ocd_wrapper.py
