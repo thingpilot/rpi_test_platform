75e7ef7a01ffeccf85fcc7d048f383b78e7a2c7e
Adam <adam@think-engineer.com>
2020-01-30 16:09:58 +0000

    Slight tidy up

--- python_ocd/targets/stm32l0.py
+++ python_ocd/targets/stm32l0.py
@@ -23,16 +23,9 @@ class STM32L0(OCDTarget):
         result = self.send('mdw 0x1FF80050 3')
 
         if result['success']:
-            print(result)
-
             if '00000000 00000000 00000000' in result['message'] or result['message'] == '':
                 result['success'] = False
 
             result['message'] = result['message'][12:38]
         
         return result
-            
-
-if __name__ == '__main__':
-    with STM32L0() as cpu:
-        print(cpu.halt())
